87bea0a0d4731366bf998e34970b98ed2c6dd1a4
Visama396 <visama396@gmail.com>
2020-07-04 22:34:46 +0200

    First command of bot

--- src/index.js
+++ src/index.js
@@ -8,4 +8,29 @@ client.once('ready', () => {
   console.log('Ready!');
 });
 
-client.l
+client.on('message', async (msg) => {
+  if (msg.channel.id === '157524499150012417') {
+    if (msg.content.toLowerCase() === 'ping') {
+      await msg.reply('pong');
+    }
+
+    const eightBall = ['As I see it, yes.',
+      'Ask again later.',
+      'Better not tell you now.',
+      'Cannot predict now.',
+      'Concentrate and ask again.',
+      'Don\'t count on it.',
+      'It is certain.',
+      'It is decidedly so.'];
+
+    let regex = /^\!8ball\s.+}/;
+
+    if (regex.exec(msg.content)) {
+      const i = Math.floor(Math.random()*eightBall.length);
+      await msg.reply(eightBall[i]);
+    }
+  }
+});
+
+client.login(process.env.BOT_TOKEN);
+
